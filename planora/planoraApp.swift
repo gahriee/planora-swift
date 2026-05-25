4b9563d6e7add701882bbea9164d11e15f3a93b4
Edgar Dollentas <edgardollentas04@gmail.com>
2026-05-26 05:58:03 +0800

    first commit

---
+++ planora/planoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  planoraApp.swift
+//  planora
+//
+//  Created by Eli on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct planoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
